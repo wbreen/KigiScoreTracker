5b198c2af56929e21b35b91324f069d6d67954bc
William Breen <wbreen@elon.edu>
2016-10-11 15:00:38 -0400

    finished the first screen layout

---
+++ KigiScoreTracker/PlayerViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KigiScoreTracker
+//
+//  Created by William Breen on 10/11/16.
+//  Copyright © 2016 William Breen. All rights reserved.
+//
+
+import UIKit
+
+class PlayerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
